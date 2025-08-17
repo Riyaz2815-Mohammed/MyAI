7423fccb8118185777754827583055e842da7b3b
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2025-08-17 07:41:46 +0530

    updated

--- agent.py
+++ agent.py
@@ -1,3 +1,6 @@
+
+
+
 from dotenv import load_dotenv
 # from livekit.agents import AutoAgent
 from livekit import agents
@@ -20,7 +23,7 @@ async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
         llm=google.beta.realtime.RealtimeModel(
         model="gemini-2.0-flash-exp",
-        voice="Leda",
+        voice="Kore",
         temperature=0.8,
         instructions=instructions1,
         )
